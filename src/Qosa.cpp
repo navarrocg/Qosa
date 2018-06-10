f59aec6774b0af26fb7ae65be6fc63e9a57aefb3
navarrocg <40135421+navarrocg@users.noreply.github.com>
2018-06-10 08:59:06 +0200

    Update Qosa.cpp

--- src/Qosa.cpp
+++ src/Qosa.cpp
@@ -1,6 +1,6 @@
 /*    This file is part of Qosa library.
 *     
-*    Created by Carlos Navarro, nov 25th, 20217
+*    Created by Carlos Navarro, navarrocg@github nov 25th, 2017
 *    
 *    Qosa is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU Lesser General Public License as 
