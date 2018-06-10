bc8044aed17a90ddc63dfe801feab022a532e915
navarrocg <40135421+navarrocg@users.noreply.github.com>
2018-06-10 09:01:05 +0200

    Add files via upload

---
+++ example/weatherSensorQosa.ino
@@ -0,0 +1,54 @@
+#include <Qosa.h>
+#include <WEMOS_SHT3X.h>
+
+#define WHOAMI "weatherSensor"
+#define PREFIX "home/multiRoom/weatherSensor"
+
+static Qosa * qosa;
+static String outTempTopic = PREFIX"/temp";
+static String outHumidityTopic = PREFIX"/hum";
+static SHT3X sht30(0x45);
+
+void setup() {
+  qosa = Qosa::gimmeAQosa(WHOAMI,PREFIX,"d5j77f70");
+  qosa->init();
+}
+
+void loop() 
+{
+  char msg[100];
+  qosa->processLoop();
+  
+ // if ( qosa->getLoopNumber() % 300 == 0 )
+ // {
+      float accTemp = 0.0;
+      float accHum = 0.0;      
+      int samples = 5;
+      
+      for ( int i = 0; i < samples; i++ )
+      {
+        sht30.get();
+        accTemp += sht30.cTemp;
+        accHum += sht30.humidity;
+        delay(20);
+      }
+      accTemp = accTemp / (float) samples;
+      accHum = accHum / (float) samples;
+      
+      dtostrf( accTemp,3,2,msg);
+      Serial.println(msg);
+      qosa->getMqttClient()->publish(outTempTopic.c_str(), msg);
+      
+      dtostrf(accHum,3,2,msg);
+      Serial.println(msg);
+      qosa->getMqttClient()->publish(outHumidityTopic.c_str(), msg);      
+ // }
+ // else
+ // {
+ //   delay(100);
+ // }
+
+  int sleepSeconds = 120;
+  // convert to microseconds
+  ESP.deepSleep(sleepSeconds * 1000000);
+}
